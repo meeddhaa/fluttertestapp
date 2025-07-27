b601675768214489422df2b98bc128aefd1ecbf2
meeddhaa <naafisa.medha@gmail.com>
2025-07-28 00:33:17 +0600

    initial setup

--- testapp/lib/main.dart
+++ testapp/lib/main.dart
@@ -88,7 +88,24 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
+        child: Container(
+          height: 80,
+          width: 80,
+          decoration: BoxDecoration(color: Colors.blue),
+          child: Center(
+            child: Text(
+              'flutter',
+
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                decoration: TextDecoration.underline,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+          ),
+
           // Column is also a layout widget. It takes a list of children and
           // arranges them vertically. By default, it sizes itself to fit its
           // children horizontally, and tries to be as tall as its parent.
@@ -102,14 +119,6 @@ class _MyHomePageState extends State<MyHomePage> {
           // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
           // action in the IDE, or press "p" in the console), to see the
           // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
